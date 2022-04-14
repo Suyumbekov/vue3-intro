b97f1d799ef22841c05dc0bbcb6058bd4b3e835d
Suyumbekov <tema.sebat@gmail.com>
2022-04-14 18:49:33 +0600

    pagination

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="app"> 
         <h1>Страница с постами</h1>
+      <my-input
+        v-model="searchQuery"
+        placeholder="Поиск...."
+      />
         <div class = "app__btns">
             <my-button 
             @click="showDialog" 
@@ -18,11 +22,19 @@
         </my-dialog>
         
         <post-list 
-            v-bind:posts="posts"
+            :posts="sortedAndSearchedPosts"
             @remove = "removePost"
             v-if="!isPostsLoading"
          />
-         <div v-else>Идет загрузка...</div>      
+         <div v-else>Идет загрузка...</div>
+        <div class="page__wrapper">
+          <div
+              v-for="page in totalPages"
+              :key="page"
+              class="page"
+          >
+            {{page}}</div>
+        </div>
     </div>
 </template>
 
@@ -30,8 +42,10 @@
     import PostForm from './components/PostForm';
     import PostList from './components/PostList';
     import axios from 'axios';
+    import MyInput from "@/components/UI/MyInput";
     export default {
         components: {
+          MyInput,
             PostList,PostForm
         },
         data() {
@@ -40,6 +54,10 @@
                 dialogVisible: false,
                 isPostsLoading: false,
                 selectedSort: '',
+                searchQuery: '',
+                page: 1,
+                limit: 10,
+                totalPages: 0,
                 sortOptions: [
                     {value: 'title', name: 'По названию'},
                     {value: 'body', name: 'По содержимому'}
@@ -60,7 +78,13 @@
             async fetchPosts(){
                 try{
                     this.isPostsLoading = true;
-                    const response = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=10");
+                    const response = await axios.get("https://jsonplaceholder.typicode.com/posts",{
+                      params: {
+                        _page: this.page,
+                        _limit: this.limit
+                      }
+                    });
+                    this.totalPages = Math.ceil(response.headers['x-total-count']/this.limit);
                     this.posts = response.data                    
                 }catch(e){
                     alert("error");
@@ -72,12 +96,15 @@
         mounted() {
             this.fetchPosts();
         },
+        computed: {
+          sortedPosts(){
+            return [...this.posts].sort((post1,post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+          },
+          sortedAndSearchedPosts(){
+            return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+          }
+        },
         watch: {
-            selectedSort(newValue){
-                this.posts.sort((post1,post2)=>{
-                    return post1[newValue]?.localeCompare(post2[newValue]);
-                })
-            }
         }
 }
 </script>
@@ -96,6 +123,15 @@
     margin: 15px 0;
     display:flex;
     justify-content: space-between;
+}
+
+.page__wrapper{
+  display: flex;
+  margin-top: 15px;
+}
+.page{
+  border: 1px solid black;
+  padding: 10px;
 }
 
 </style>
